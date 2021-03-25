f255f794649dd31b6559ed9bec7f2a421a51faad
Dmytro Kovalov <dkovalyov@aer.de>
2021-03-25 20:41:29 +0200

    [naprobu-3] Test

--- app/Http/Controllers/Admin/Project/ProjectController.php
+++ app/Http/Controllers/Admin/Project/ProjectController.php
@@ -38,7 +38,7 @@ class ProjectController extends Controller implements iAdminController
 	//Список всех проектов
 	public function all(){
         $countryCollection = CountryCollection::buildCollection();
-        dd($countryCollection->first()));
+        dd($countryCollection->first());
 
 		$projects = Project::with(['category','status','requests'])
 			->withCount(['requests'])

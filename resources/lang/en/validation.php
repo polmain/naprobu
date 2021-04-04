996452efbd0043a1093b286dd3a5ed8e5a5ae55e
Dmytro Kovalov <dkovalyov@aer.de>
2021-04-04 13:17:21 +0300

    [naprobu-7] Part 5 (Search to Validation)

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'required'			=>	'This field is required',
+	'recaptcha'			=>	'Check that you are not a robot',
+	'maxlength'			=>	'Exceeded character limit',
+	'minlength'			=>	'Minimum {0} characters',
+	'email'				=>	'Incorrect email address',
+];

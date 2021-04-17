b8ec9d25c7835fafc05d5ae13b66c2ebb8dda2b8
Dmytro Kovalov <dkovalyov@aer.de>
2021-04-17 20:23:22 +0300

    [naprobu-10] Translate mains page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,7 +13,9 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
-    @yield('head')
+    @foreach($alternativeUrls as $lang => $alternet_url)
+        <link rel="alternate" href="{{$alternet_url}}" hreflang="{{ $lang }}" />
+    @endforeach
 
 
     <script src="{{ asset("/public/js/adriver.core.2.js")}}" ></script>

cc824e9c02f012f9b6ef5210d5f7cb7027d3c9c6
Dmytro Kovalov <dkovalyov@aer.de>
2021-04-18 12:26:36 +0300

    [naprobu-10] Translate errors page

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,8 +1,4 @@
 @extends('layouts.main')
-@section('lang_href',$alternet_url)
-@section('head')
-    <link rel="alternate" href="{{$alternet_url}}" hreflang="{{(App::getLocale() == 'ru')?'uk':'ru'}}-UA" />
-@endsection
 @section('content')
     <div class="container">
         <div class="row">

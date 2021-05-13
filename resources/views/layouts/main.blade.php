d715625287e87847d07e29c5f774b57a15516a53
Dmytro Kovalov <dkovalyov@aer.de>
2021-05-13 19:01:00 +0300

    [naprobu-15] Update to current PROD state

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -13,9 +13,11 @@
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
+    @if(isset($alternativeUrls))
     @foreach($alternativeUrls as $lang => $alternet_url)
         <link rel="alternate" href="{{$alternet_url}}" hreflang="{{ $lang }}" />
     @endforeach
+    @endif
 
     @yield('head')
 

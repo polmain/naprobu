57ccbe3a9dcbe116584c692b114593fc7712258e
Dmytro Kovalov <dkovalyov@aer.de>
2021-05-16 17:46:17 +0300

    [naprobu-21] change viber logic

--- database/migrations/2021_05_16_173514_change_logic_on_users_table.php
+++ database/migrations/2021_05_16_173514_change_logic_on_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddNewFormStatusToUsersTable extends Migration
+class ChangeLogicOnUsersTable extends Migration
 {
     /**
      * Run the migrations.

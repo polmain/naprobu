7021b6d4d7eb9c0c1efe6c263a4bede8aaa61678
Dmytro Kovalov <dkovalyov@aer.de>
2021-05-30 17:38:20 +0300

    [naprobu-24] fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -196,7 +196,11 @@
             });
         });
 
-
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
         $.ajax({
             method: 'POST',
             dataType: 'json',

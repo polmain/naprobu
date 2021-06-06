f59d22a42ae601bb0251d39436e36672317dc0a1
Dmytro Kovalov <dkovalyov@aer.de>
2021-06-06 19:34:44 +0300

    [naprobu-22] fix

---
+++ resources/lang/ru/work.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'top_manager'      =>	'Топ менеджер',
+	'middle_manager'   =>	'Руководитель среднего звена',
+	'executor'	        =>	'Исполнитель',
+	'business_owner'   =>	'Владелец бизнеса',
+	'self_employed'    =>	'Частный предприниматель',
+];
